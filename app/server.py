1f81dccbb67678e3b1ba624a0d14b46280df9b1c
Muhammad Ali <malitariq5324@gmail.com>
2026-01-10 18:29:47 +0500

    - imported a library to validate urls

--- app/server.py
+++ app/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from validators import url as url_validate
 
 app = Flask(__name__)
 @app.route('/')
@@ -6,8 +7,11 @@ def homePage():
     return render_template(template_name_or_list=["homePage.html"])
 
 @app.route("/encodeUrl")
-def encodeAndSave(url=""):
-    return
+def encodeAndSave(URL=""):
+    if url_validate(URL):
+        pass
+    else:
+        pass
 
 if __name__ == "__main__":
     app.run(debug=True)
