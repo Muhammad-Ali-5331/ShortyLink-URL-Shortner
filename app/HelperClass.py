b29ab41f54a103b553710d7a283a22e2d1732706
Muhammad Ali <malitariq5324@gmail.com>
2026-01-10 19:42:57 +0500

    feat: imported just the needed methods from random library

--- app/HelperClass.py
+++ app/HelperClass.py
@@ -1,15 +1,15 @@
-import random
+from random import randint,shuffle,choice
 from string import digits,ascii_letters
 
 class EncoderDecoder:
     def __init__(self):
         self.Alphabets = list(digits + ascii_letters)
-        for _ in range(random.randint(10,20)):
-            random.shuffle(self.Alphabets)
+        for _ in range(randint(10,20)):
+            shuffle(self.Alphabets)
         self.MAP = {}
 
     def getCode(self):
-        return "".join(random.choice(self.Alphabets) for _ in range(random.randint(5,8)))
+        return "".join(choice(self.Alphabets) for _ in range(randint(5,8)))
 
     def encodeUrl(self,long_Url):
         short_code = self.getCode()
