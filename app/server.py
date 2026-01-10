8bac7d8b724fc578c0a5e0b2ec9d9a868af69888
Muhammad Ali <malitariq5324@gmail.com>
2026-01-10 18:21:24 +0500

    - remove unused library - Set up the encode url end

--- app/server.py
+++ app/server.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
-from werkzeug.utils import redirect
 
 app = Flask(__name__)
 @app.route('/')
 def homePage():
     return render_template(template_name_or_list=["homePage.html"])
+
+@app.route("/encodeUrl")
+def encodeAndSave(url=""):
+    return
 
 if __name__ == "__main__":
     app.run(debug=True)
